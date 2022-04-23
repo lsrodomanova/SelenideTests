364429ff4694132a6d1bb5e8accd467c634c5b68
Lyubov Rodomanova <lrodomanova81@gmail.com>
2022-04-23 20:13:07 +0300

    Initial commit

---
+++ src/test/java/com/lubl/SelenideWikiTest.java
@@ -0,0 +1,19 @@
+package com.lubl;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class SelenideWikiTest {
+
+    @Test
+    void parseSoftAssertionsPage() {
+        Selenide.open("https://github.com/selenide/selenide");//Открыть страницу Selenide в GitHub
+        $("#wiki-tab").click();//Перйти в раздел Wiki
+        $(".markdown-body").find(byText("Soft assertions")).click();//Открыть страницу Soft Asserions
+        $("#wiki-wrapper").shouldHave(text("JUnit5 extension"), text("Using JUnit5 extend test class"));//Проверить, что есть код для JUnit5
+    }
+}
